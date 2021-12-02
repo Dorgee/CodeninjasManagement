fbcf3486285e235e9591ffe681b57c07e6ed1d1e
Dorgee Lama <dorgeelama@Dorgees-MacBook-Pro.local>
2021-12-01 18:48:20 -0700

    added connection code

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const account = require('./router/accountrouter.js');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 const PORT = 4000;
 
+app.use('/accounts', account);
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+});
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+mongoose.connection.on('error', (err) => {
+  console.log('err', err);
+});
+
+mongoose.connection.on('connected', (err, res) => {
+  console.log('mongoose is connected');
 });
