04975cc307c7f3903fb606010cb2348fd8409c29
Dorgee <71479348+Dorgee@users.noreply.github.com>
2021-12-10 19:27:02 -0700

    finished routes for scratch and roblox

--- router/robloxaccount.js
+++ router/robloxaccount.js
@@ -2,22 +2,62 @@ const express = require('express');
 const router = express.Router();
 const Roblox = require('../models/Roblox');
 
+router.get('/all', async (req, res) => {
+  try {
+    const robloxAccounts = await Roblox.find();
+    //  console.log(posts);
+    // send back the posts as an json object
+    res.json(robloxAccounts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
 
-router.post('/', async (req, res ) =>{
-    const {username, password} = req.body;
-    const robloxAccount = new Roblox({
-        username: username,
-        password: password
-    })
-    try{
+router.post('/', async (req, res) => {
+  const { username, password } = req.body;
+  const robloxAccount = new Roblox({
+    username: username,
+    password: password,
+  });
+  try {
     const savedAccount = await robloxAccount.save();
     res.json(savedAccount);
-    }
-    catch(err){
-        console.log(err);
-    }
-    
-})
+  } catch (err) {
+    console.log(err);
+  }
+});
 
+// find specific account
+router.get('/:accountId', async (req, res) => {
+  try {
+    const robloxAccount = await Roblox.findById(req.params.accountId);
+    res.json(robloxAccount);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+// edit a sepcific account
+router.patch('/:accountId', async (req, res) => {
+  try {
+    const updatedAccount = await Roblox.findByIdAndUpdate(
+      req.params.accountId,
+      req.body
+    );
+    res.json(updatedAccount);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// delete a specific account
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedAccount = await Roblox.findByIdAndDelete(id);
+    res.json(deletedAccount);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
--- router/scratchaccount.js
+++ router/scratchaccount.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router();
 const Scratch = require('../models/Scratch');
 
+router.get('/all', async (req, res) => {
+  try {
+    const scratchAccounts = await Scratch.find();
+    //  console.log(posts);
+    // send back the posts as an json object
+    res.json(scratchAccounts);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
 router.post('/', async (req, res) => {
   const { username, password } = req.body;
   const scratchAccount = new Scratch({
@@ -11,6 +22,39 @@ router.post('/', async (req, res) => {
   try {
     const savedAccount = await scratchAccount.save();
     res.json(savedAccount);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// find specific account
+router.get('/:accountId', async (req, res) => {
+  try {
+    const scratchAccount = await Scratch.findById(req.params.accountId);
+    res.json(scratchAccount);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+// edit a sepcific account
+router.patch('/:accountId', async (req, res) => {
+  try {
+    const updatedAccount = await Scratch.findByIdAndUpdate(
+      req.params.accountId,
+      req.body
+    );
+    res.json(updatedAccount);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// delete a specific account
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedAccount = await Scratch.findByIdAndDelete(id);
+    res.json(deletedAccount);
   } catch (err) {
     console.log(err);
   }
